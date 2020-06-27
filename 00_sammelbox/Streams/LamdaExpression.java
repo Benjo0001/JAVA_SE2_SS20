632995d073d44c15bf0789fe33ae7968870fa98c
Benjo0001 <ben.hoppe6.0@gmail.com>
2020-06-27 14:54:37 +0200

    ArrayList repetition informatikZentrale

---
+++ 00_sammelbox/Streams/LamdaExpression.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * https://www.youtube.com/watch?v=gpIUfj3KaOc&list=PLqq-6Pq4lTTa9YGfyhyW2CqdtW9RtY-I3
+ * 
+ * && 
+ * https://www.youtube.com/watch?v=lIXs4Y8sJCk
+ */
+public class LamdaExpression {
+    
+}
---
+++ 10_collections_generics/array_list/ArrayListMethods_informatikZentrale/ArrayListMethods.java
@@ -0,0 +1,72 @@
+/**
+ * https://www.youtube.com/watch?v=zotAs4v9jH4     - InformatikZentrale
+ * 
+ * Listen - Eigenschaften
+ * - geordnete Elemente
+ * - Elemente können mehrfach auftreten
+ * 
+ */
+import java.util.ArrayList;
+public class ArrayListMethods {
+    public static void main(String[] args) {
+        ArrayList<String> friendsList = new ArrayList<>();
+
+        // Element einfügen, an letzter Stelle
+        friendsList.add("Peter");
+        friendsList.add("Klausi");
+        friendsList.add("Jochen");
+
+        // Element "dazwischen" einfügen auf Index-Stelle
+        friendsList.add(1, "Heinrich");
+
+        // Elemente ändern
+        friendsList.set(0, "Pete");
+
+        //Elemente entfernen
+        friendsList.remove("Klausi");   // mit Wert
+        friendsList.remove(0);          // mit Index
+
+
+    
+        //Elemente ausgeben an Index-Stelle
+        String var = friendsList.get(0);
+        
+        System.out.println(friendsList);
+        
+
+        ArrayList<Integer> plz = new ArrayList<>();
+        plz.add(15745);
+        plz.add(10178);
+        plz.add(10777);
+
+        ArrayList<String> place = new ArrayList<>();
+        place.add("Wildau");
+        place.add("Berlin-Mitte");
+        place.add("Berlin-Schöneberg");
+
+        // Alle Orte ausgeben
+        for (String p : place) {
+            System.out.println(p);
+        }
+        
+        // Alle Orte mit PLZ ausgeben:
+        for (int i = 0; i < place.size(); i++) {
+            System.out.println(plz.get(i) + "\t"+ place.get(i));
+            
+        }
+
+        ArrayList<Integer> testInt = new ArrayList<>();
+        testInt.add(12);
+        testInt.add(3);
+        testInt.add(4);
+        testInt.add(1);
+        testInt.remove(1);                  // removes the value called by index
+        testInt.remove( (Integer)(1));      // removes the value called by the value
+        
+        System.out.println(testInt);
+
+        
+    }
+
+
+}
