0926dea0f48976f58c12f717d5b6c147c8c7549c
Benjo0001 <ben.hoppe6.0@gmail.com>
2020-06-27 19:37:37 +0200

    linkedList start, vorher im falschen Ordner gestaged

---
+++ 07_packages/StaticImports/StaticImports.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 
+ * https://www.youtube.com/watch?v=kHWcA4X2anE
+ */
+
+import static java.lang.System.out;
+
+public class StaticImports {
+    public static void main(String[] args) {
+        out.println("Hello");
+    }
+}
