6dcbf546bbfe8190879a339369adb38665a29f0e
Benjo0001 <ben.hoppe6.0@gmail.com>
2020-06-28 09:19:52 +0200

    Ergänzung u19 Exceptions

--- JAVA_SE_2_hendrix/uebungen_SE2_hendrix/u19/ConvertBinaryTest.java
+++ JAVA_SE_2_hendrix/uebungen_SE2_hendrix/u19/ConvertBinaryTest.java
@@ -1,30 +1,35 @@
 /**
     Schreiben Sie eine Klasse ConvertBinaryTest zum Testen der Umwandlung.
+
+    ähnliche Beispiellösung im Jav9 Herd-Heft Seite 198 (Kapitel 14.7 Übungen)
  */
 
 public class ConvertBinaryTest {
     public static void main(String[] args) {
-        // try {
-        //     System.out.println(ConvertBinary.ConvertBinaryNumberString("001010101") + "\n");    
-        // } catch (NoBinaryNumberException e) {
-        //     e.printStackTrace();
-        // }
-        // try {
-        //     System.out.println(ConvertBinary.ConvertBinaryNumberString("00101g101") + "\n");
-        // } catch (NoBinaryNumberException e) {
-        //     e.printStackTrace();
-        // }
-        // try {
-        //     System.out.println(ConvertBinary.ConvertBinaryNumberString("0001") + "\n");    
-        // } catch (NoBinaryNumberException e) {
-        //     e.printStackTrace();
-        // }
-        // try {
-        //     System.out.println(ConvertBinary.ConvertBinaryNumberString("1111010o1") + "\n");    
-        // } catch (NoBinaryNumberException e) {
-        //     e.printStackTrace();
-        // }
+        try {
+            System.out.println(ConvertBinary.ConvertBinaryNumberString("001010101") + "\n");    
+        } catch (NoBinaryNumberException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(ConvertBinary.ConvertBinaryNumberString("00101g101") + "\n");
+        } catch (NoBinaryNumberException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(ConvertBinary.ConvertBinaryNumberString("0001") + "\n");    
+        } catch (NoBinaryNumberException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(ConvertBinary.ConvertBinaryNumberString("1111010o1") + "\n");    
+        } catch (NoBinaryNumberException e) {
+            e.printStackTrace();
+        }
+
         System.out.println(ConvertBinary.ConvertBinaryNumberString("00101g101") + "\n");
+        // ohne try catch block wird die NoBinaryNumberException nicht gefangen und stößt bis zur main vor -> Programm stoppt hier
+        System.out.println(ConvertBinary.ConvertBinaryNumberString("00101101") + "\n");
 
     }
 }
